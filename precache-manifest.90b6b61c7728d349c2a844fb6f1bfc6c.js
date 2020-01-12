d67722a7f42480ff497783274bc1c0edf98dcbee
Akshay-47 <akshay.lokray47@gmail.com>
2020-01-12 19:06:04 +0530

    Updates

---
+++ precache-manifest.90b6b61c7728d349c2a844fb6f1bfc6c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d53bb770e1764289b9061826c88b7a0",
+    "url": "/Commerce-Factory/index.html"
+  },
+  {
+    "revision": "a60fa1b097ddddffbc48",
+    "url": "/Commerce-Factory/static/css/main.a37c35a5.chunk.css"
+  },
+  {
+    "revision": "b8ea074a7379246f800e",
+    "url": "/Commerce-Factory/static/js/2.d311027e.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/Commerce-Factory/static/js/2.d311027e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a60fa1b097ddddffbc48",
+    "url": "/Commerce-Factory/static/js/main.c957de81.chunk.js"
+  },
+  {
+    "revision": "caa61e1a1a13100ee8cd",
+    "url": "/Commerce-Factory/static/js/runtime-main.4742ef14.js"
+  }
+]);
